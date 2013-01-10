7a85f39a9d81dd3337dca68a2f2f3c80834d89d9
Andy Park <sohocoke@gmail.com>
2013-01-10 02:21:13 +0100

    catch if #watch_src goes funny.

--- application_delegate.rb
+++ application_delegate.rb
@@ -6,27 +6,32 @@ TOPLEVEL_OBJECT = self
 class ApplicationDelegate
 	#include DefaultsAccess
 	
+	# TODO if app not customised yet, create a new session on app load.
+	
   def newTopLevelObjectSession(sender)
-		#	watch_if default( :console_on_launch ) do
+  	# TODO refactor into a more variation-friendly place, like IRBWindowController.rc
 
-			# global var for handy access when interactive.
-			$tool ||= IRBTool.new
+		# global var for handy access when interactive.
+		$tool ||= IRBTool.new
 
-			@@irb_global_inited ||= false
-      unless @@irb_global_inited
-      	
-      	# load irb files.
-        $tool.load_all
-        @@irb_global_inited = true
-      
-      	# watch for hotloading.
-      	$tool.watch_src
+		@@irb_global_inited ||= false
+    unless @@irb_global_inited
+    	
+    	# load irb files.
+      $tool.load_all
+      @@irb_global_inited = true
+    
+    	# watch for hotloading.
+			begin
+				$tool.watch_src
+			rescue Exception => e
+				qb_report e
+				puts "not watching folder due to last exception."
+			end
 
-      end
+    end
 
-	    irb(TOPLEVEL_OBJECT, TOPLEVEL_BINDING.dup)
-	    
-	  # end
+    irb(TOPLEVEL_OBJECT, TOPLEVEL_BINDING.dup)
   end
 
 end
